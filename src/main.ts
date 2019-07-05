ed95b03cb3e600ad46a58c4421d1b9d9cf9f8e89
Onyeka Ijeh <onyekaijeh@gmail.com>
2019-07-05 13:30:52 +0100

    feat: enable gzip compression

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(helmet());
+  app.use(compression());
   await app.listen(4000);
 }
 bootstrap();
